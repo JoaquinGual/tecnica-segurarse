17b68fdf553ab27aced36b327df3eb04b9bc6453
Joaquin Gual <joaquingual1@gmail.com>
2022-04-29 16:48:45 -0300

    ANDA RE PRO

--- src/components/FormGroup/FormGroup.jsx
+++ src/components/FormGroup/FormGroup.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import {Formik, Field, Form } from 'formik'
+import React, { useState } from 'react'
+import { Formik, Field, Form } from 'formik'
 import * as Yup from 'yup';
 
 import Swal from 'sweetalert2';
@@ -10,58 +10,65 @@ import { usePostBase64 } from '../../hooks/usePostBase64';
 
 
 export const FormGroup = () => {
-  
-  const {addUser} = useAddUser();
-  const {postBase64} = usePostBase64();
-  
+
+  const { addUser } = useAddUser();
+  const { postBase64 } = usePostBase64();
+  const [aux, setAux] = useState();
+
   const initialValues = {
     txtNombre: "",
     txtApellido: ""
-}
-const validationSchema = Yup.object({
+  }
+  const validationSchema = Yup.object({
     txtNombre: Yup.string().min(3).required('Nombre Requerido'),
     txtApellido: Yup.string().min(3).required('Apellido Requerido')
-})
+  })
 
 
   return (
     <>
+
       <Formik initialValues={initialValues} validationSchema={validationSchema}
-      onSubmit={async (values, formikBag) => {
-        try {
-          const {data} = await postBase64(values.txtNombre, values.txtApellido);
-          console.log(data);
+        onSubmit={async (values, formikBag) => {
+          try {
+            const { data } = await postBase64(values.txtNombre, values.txtApellido);
+            console.log(data)
+            setAux(data)
             await addUser(values.txtNombre, values.txtApellido)
             Swal.fire("Ok", "Se agrego ok!", "success");
             formikBag.resetForm();
-        } catch (error) {
+            return data;
+
+          } catch (error) {
             Swal.fire("Error", "Error al registrar usuario")
-        }
-    }}>
+          }
+        }}>
+        
         <Form>
           <div className='container mt-2 d-flex justify-content-between'>
             <div className='col-md-4'>
-            <Field name="txtNombre"
-            type="text"
-            className="form-control"
-            placeholder="Nombre" 
-            />
+              <Field name="txtNombre"
+                type="text"
+                className="form-control"
+                placeholder="Nombre"
+              />
             </div>
             <div className='col-md-4'>
-            <Field name="txtApellido"
-            type="text"
-            className="form-control"
-            placeholder="Apellido" 
-            />
+              <Field name="txtApellido"
+                type="text"
+                className="form-control"
+                placeholder="Apellido"
+              />
             </div>
             <div className='col-md-3'>
               <button type="submit" className='btn btn-dark'>Añadir Usuario</button>
             </div>
           </div>
-          
+
         </Form>
       </Formik>
-      <TableUsers/>
+      <h1>{aux}</h1>
+      <TableUsers />
     </>
   )
 }
--- src/hooks/usePostBase64.jsx
+++ src/hooks/usePostBase64.jsx
@@ -1,27 +1,22 @@
 import { useMutation, useQueryClient } from 'react-query';
 import axios from 'axios';
-import { useState } from 'react';
-import { useEffect } from 'react';
+
 
 export const usePostBase64 = () => {
     const queryClient = useQueryClient();
-    const [val, setVal] = useState();
 
-    // const getAnswer = async () => {
-    //     const data = await axios(`https://localhost:44397/Segurarse/User/GetBase64?nombre=${nombre}&apellido=${apellido}`);
-    //     setVal(data.data);
-    // };
-    // getAnswer();
-    const mutation = useMutation(({ nombre, apellido }) => {
+    let data2 = '';
+
+    const mutation = useMutation(async ({ nombre, apellido }) => {
         const datos = JSON.stringify({ nombre, apellido });
 
 
 
 
-
-        return axios.post(`https://localhost:44397/Segurarse/User/PostBase64`, datos, {
-            headers: { 'Content-Type': 'application/json', 'Authorization': val }
+        return await axios.post(`https://localhost:44397/Segurarse/User/PostBase64`, datos, {
+            headers: { 'Content-Type': 'application/json', 'Authorization': data2 }
         });
+
     },
         {
             onSuccess: () => { queryClient.invalidateQueries('getanswer'); },
@@ -31,19 +26,27 @@ export const usePostBase64 = () => {
         try {
 
             const data = await axios.get(`https://localhost:44397/Segurarse/User/GetBase64?nombre=${nombre}&apellido=${apellido}`);
-            console.log(data.data)
-
-            setVal(data.data);
-            console.log(val);
 
 
-            await mutation.mutateAsync({ nombre, apellido });
+
+            data2 = data?.data;
+
+          
+
+
+            const response = await mutation.mutateAsync({ nombre, apellido });
+            
+
+
             return {
-                data: mutation.data?.data,
+                data: response?.data,
                 error: mutation.isError,
                 loading: mutation.isLoading
             };
+
+
         } catch (error) {
+
             return {
                 data: mutation.data?.data,
                 error: true,
